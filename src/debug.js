02f170ea3e7eef5c3af7219ac155fd1b1381b646
luo <luokobe@gmail.com>
2013-06-27 22:07:03 +0800

    add switcher to debug;add setPrefix method;update test.html

--- src/debug.js
+++ src/debug.js
@@ -4,9 +4,10 @@ var debug = (function(){
         UNDEFINED = "undefined",
         _levels = ["log","debug","info","warn","error"],
         _length = _levels.length,
-        _emptyFunc = function(){},
+        _emptyFunc = function() {},
         console = window.console,
-        i;
+        _prefix = '[Diigo]:',
+         i;
 
     function _isConsoleFuntionExist(level){
         return typeof console[level] === UNDEFINED;
@@ -16,38 +17,58 @@ var debug = (function(){
         return typeof console === UNDEFINED;
     }
 
-    function _getConsoleFunction(level){
+    function _getConsoleFunction(level, _prefix){
         if (console[level].bind === undefined) {
             if (Function.prototype.bind === undefined) {
                 return function(){
-                    console[level].apply(console,arguments);
+                    var args = Array.prototype.slice.apply(arguments);
+                    args.unshift(_prefix);
+                    console[level].apply(console,args);
                 };
             } else {
-                return Function.prototype.bind.call(console[level],console);
+                return Function.prototype.bind.call(console[level],console,_prefix);
             }
         } else {
-            return console[level].bind(console);
+            return console[level].bind(console,_prefix);
         }
 
     }
 
-    function _generateMethod(level){
+    function _generateMethod(level, _prefix){
         if (_isConsoleExist()) {
             return _emptyFunc;
         } else if (_isConsoleFuntionExist(level)) {
-            return _getConsoleFunction('log') || _emptyFunc;
+            return _getConsoleFunction('log', _prefix) || _emptyFunc;
         } else {
-            return _getConsoleFunction(level);
+            return _getConsoleFunction(level, _prefix);
         }
     }
-    //TODO: add a switch
-    //TODO: add prefix
 
+    sel.init = function() {
+        for (i = 0;i < _length;i++) {
+            var level = _levels[i];
+            if (_debug === true) {
+                sel[level] = _generateMethod(level, _prefix);
+            } else {
+                sel[level] = _emptyFunc;
+            }
 
-    for (i = 0;i < _length;i++) {
-        var level = _levels[i];
-        sel[level] = _generateMethod(level);
-    }
+        }
+    };
+
+    // switcher for debug
+    sel.disable = function() {
+        _debug = false;
+        this.init();
+    };
+
+    // set prefix to console
+    sel.setPrefix = function(prefix) {
+        _prefix ='[' + prefix + ']:';
+        this.init();
+    };
+
+    sel.init();
 
     return sel;
 }());
